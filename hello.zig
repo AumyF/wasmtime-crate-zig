caeb7378f08680959732169784ee47b45fb5b31b
Aumy <aumy@outlook.jp>
2024-03-21 00:33:06 +0900

    CBOR!!!!!!!!!!!!!!!

--- hello.zig
+++ hello.zig
@@ -1,5 +1,23 @@
 const std = @import("std");
+const cbor = @import("cbor");
 
 export fn hello() void {
     std.debug.print("Hello, zig from wasmtime embedded in rust!\n", .{});
+}
+
+export fn test_cbor() u64 {
+    const allocator = std.heap.wasm_allocator;
+    var str = std.ArrayList(u8).init(allocator);
+    // defer str.deinit();
+
+    cbor.stringify(.{ .name = "John Doe" }, .{}, str.writer()) catch {
+        unreachable;
+    };
+
+    return string_to_ptr(str.items);
+}
+
+fn string_to_ptr(s: []const u8) u64 {
+    const ptr: u64 = @intFromPtr(s.ptr);
+    return ptr << 32 | s.len;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use wasi_common::pipe::WritePipe;
 use wasi_common::sync::WasiCtxBuilder;
 use wasmtime::*;
 
@@ -11,30 +12,57 @@ fn main() -> Result<()> {
     let mut linker = Linker::new(&engine);
     wasi_common::sync::add_to_linker(&mut linker, |s| s)?;
 
-    let wasi = WasiCtxBuilder::new()
-        .inherit_stdio()
-        .inherit_args()?
-        .inherit_env()?
-        .build();
-    // All wasm objects operate within the context of a "store". Each
-    // `Store` has a type parameter to store host-specific data, which in
-    // this case we're using `4` for.
-    let mut store = Store::new(&engine, wasi);
-    let module = Module::from_file(&engine, "zig-out/lib/hello.wasm")?;
-    linker.module(&mut store, "", &module)?;
+    {
+        let wasi = WasiCtxBuilder::new()
+            .inherit_stdio()
+            .inherit_args()?
+            .inherit_env()?
+            .build();
+        // All wasm objects operate within the context of a "store". Each
+        // `Store` has a type parameter to store host-specific data, which in
+        // this case we're using `4` for.
+        let mut store = Store::new(&engine, wasi);
+        let module = Module::from_file(&engine, "zig-out/lib/hello.wasm")?;
+        linker.module(&mut store, "", &module)?;
 
-    linker
-        .get(&mut store, "", "hello")
-        .unwrap()
-        .into_func()
-        .unwrap()
-        .typed::<(), ()>(&store)?
-        .call(&mut store, ())?;
+        let p = linker
+            .get(&mut store, "", "test_cbor")
+            .unwrap()
+            .into_func()
+            .unwrap()
+            .typed::<(), u64>(&store)?
+            .call(&mut store, ())?;
 
+        let memory = linker
+            .get(&mut store, "", "memory")
+            .unwrap()
+            .into_memory()
+            .unwrap();
+
+        let ptr = (p >> 32) as usize;
+        let len = (p & 0xffffffff) as usize;
+
+        let mut buf = Vec::new();
+        buf.resize(len, 0);
+        memory.read(&store, ptr, &mut buf).unwrap();
+
+        let a: ciborium::Value = ciborium::from_reader(&buf[..])?;
+        let bytes = a.as_map().unwrap().get(0).unwrap().1.as_bytes().unwrap();
+        let name = String::from_utf8(bytes.to_vec())?;
+
+        println!("len: {len}");
+        println!("{:?}", a);
+        println!("name: {name}");
+    }
     // let hello = instance.get_typed_func::<(), ()>(&mut store, "hello")?;
 
     // And finally we can call the wasm!
     // hello.call(&mut store, ())?;
 
     Ok(())
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct Foo {
+    name: String,
 }
